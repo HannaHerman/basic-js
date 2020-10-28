d053b8489b6de86618b00747e339f45f991a4fd2
HannaHerman <mrs.hanna.herman@gmail.com>
2020-10-28 21:48:56 +0300

    docs: solved what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+  
   if (date === undefined){
     return "Unable to determine the time of year!";
   } else if (!date.getTime()) {
