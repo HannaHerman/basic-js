51401ec17fef7df9810a3f1930a62977505736a2
HannaHerman <mrs.hanna.herman@gmail.com>
2020-10-28 16:50:07 +0300

    docs: solved transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let resArr = [];
+  if(Array.isArray(arr)) {
+    let counter = 0;
+    while (counter < arr.length) {
+      if (arr[counter] === '--discard-next') {
+        if (arr[counter + 2] === "--double-prev" || arr[counter + 2] === "--discard-prev") {
+          counter++;
+        }
+        counter += 2;
+      } else if (arr[counter] === '--discard-prev') {
+        if (counter !== 0) {
+          resArr.pop();
+          counter++;
+        } else {
+          counter++;
+        }
+      } else if (arr[counter] === '--double-next') {
+        if ((counter + 1) === arr.length) {
+          counter++;
+        } else {
+          resArr.push(arr[counter + 1]);
+          counter++;
+        };
+      } else if (arr[counter] === '--double-prev') {
+        if (counter === 0) {
+          counter++;
+        } else {
+          resArr.push(arr[counter - 1]);
+          counter++;
+        };
+      } else {
+        resArr.push(arr[counter]);
+        counter++;
+      }
+    }
+    
+  } else {
+    throw new Error();
+  }
+  return resArr;
+}
